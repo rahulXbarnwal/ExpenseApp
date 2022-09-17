13b44ceae70b99c492d382e9a75b19836e4efc26
rahulXbarnwal <rahulbarnwalofficial@gmail.com>
2022-09-17 20:06:43 +0530

    Finalized

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setShowForm(false);
   };
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {/* <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> */}
+      {
+        !showForm ?
+          (<button
+            className='new-expense__actions'
+            onClick={() => { setShowForm(true) }}>Add New Expense
+          </button>
+          ) : ( <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> )
+      }
     </div>
   );
 };
